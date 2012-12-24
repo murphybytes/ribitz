8d1393a3d1b823f74a9ce537eb3985d5329e7119
John Murphy <murphybytes@gmail.com>
2012-12-24 08:32:05 -0600

    adding to base framework

---
+++ lib/ribitz/router/router.rb
@@ -0,0 +1,13 @@
+
+module Ribitz
+  class Router
+    def initialize( opts )
+    end
+
+    def run
+      # publish heartbeats
+      # publish binding information
+      # listen for collab messages and publish them on appropriate push socket
+    end
+  end
+end
